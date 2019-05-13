4790c7e4e77e37178a8d6927cdcc041dafbf6022
Victor Sanchez <vhsv90@gmail.com>
2019-05-13 10:14:34 -0600

    ServiceExtension class added

---
+++ ModusCreate/Extensions/ServiceExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ModusCreate.Extensions
+{
+    public static class ServiceExtensions
+    {
+
+        public static void ConfigureCors(this IServiceCollection service)
+        {
+            service.AddCors(options => {
+                options.AddPolicy("CorsPolicy", 
+                    builder => builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials()
+                );
+            });
+        }
+
+        public static void ConfigureIISIntegration(this IServiceCollection service)
+        {
+            service.Configure<IISOptions>(options => {
+                // add configure here
+            });
+        }
+
+
+    }
+}
