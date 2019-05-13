3237bc50bee27371363e57a8dfa642e0c989a468
Victor Sanchez <vhsv90@gmail.com>
2019-05-13 10:10:22 -0600

    News and Subscription models added

---
+++ ModusCreate/Models/News.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ModusCreate.Models
+{
+    public class News
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long NewsId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Category { get; set; }
+
+        public DateTime Date { get; set; }
+    
+    }
+}
---
+++ ModusCreate/Models/Subscription.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ModusCreate.Models
+{
+    public class Subscription
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string Category { get; set; }
+    }
+}
