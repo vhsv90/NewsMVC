885fce2f99ca7bcbe9a8f000b3d7871077ef9653
Victor Sanchez <vhsv90@gmail.com>
2019-05-13 10:14:17 -0600

    DB Manager for News and Subscription added

---
+++ ModusCreate/Models/Manager/NewsManager.cs
@@ -0,0 +1,65 @@
+﻿using ModusCreate.Models.DB;
+using ModusCreate.Models.Repository;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ModusCreate.Models.Manager
+{
+    public class NewsManager : IDataRepository<News>
+    {
+        public readonly NewsContext _newsContext;
+
+        public NewsManager(NewsContext newsContext)
+        {
+            _newsContext = newsContext;
+        }
+
+        public void Add(News entity)
+        {
+            _newsContext.News.Add(entity);
+            _newsContext.SaveChanges();
+        }
+
+        public void Delete(News entity)
+        {
+            _newsContext.News.Remove(entity);
+            _newsContext.SaveChanges();
+        }
+
+        public News Get(long id)
+        {
+            return _newsContext.News.FirstOrDefault(x => x.NewsId == id);
+        }
+
+        public IEnumerable<News> GetAll()
+        {
+            return _newsContext.News.ToList();
+        }
+
+        public IEnumerable<News> GetAllByCategory(string category)
+        {
+            return _newsContext.News.Where(x => x.Category.Equals(category)).ToList();
+        }
+
+        public IEnumerable<News> Search(string searchTerm)
+        {
+            var result = _newsContext
+                .News
+                .Where(x => x.Title.Contains(searchTerm) || x.Description.Contains(searchTerm))
+                .ToList()
+                ;
+
+            return result;
+        }
+
+        public void Update(News dbEntity, News entity)
+        {
+            dbEntity.Category = entity.Category;
+            dbEntity.Date = entity.Date;
+            dbEntity.Description = entity.Description;
+            dbEntity.Title = entity.Title;
+
+            _newsContext.SaveChanges();
+        }
+    }
+}
---
+++ ModusCreate/Models/Manager/SubscriptionsManager.cs
@@ -0,0 +1,57 @@
+﻿using ModusCreate.Models.DB;
+using ModusCreate.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModusCreate.Models.Manager
+{
+    public class SubscriptionsManager : IDataRepository<Subscription>
+    {
+
+        public readonly NewsContext _newsContext;
+
+        public SubscriptionsManager(NewsContext newsContext)
+        {
+            _newsContext = newsContext;
+        }
+
+        public void Add(Subscription entity)
+        {
+            _newsContext.Subscriptions.Add(entity);
+            _newsContext.SaveChanges();
+        }
+
+        public void Delete(Subscription entity)
+        {
+            _newsContext.Subscriptions.Remove(entity);
+            _newsContext.SaveChanges();
+        }
+
+        public Subscription Get(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Subscription> GetAll()
+        {
+            return _newsContext.Subscriptions.ToList();
+        }
+
+        public IEnumerable<Subscription> GetAllByCategory(string category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Subscription> Search(string searchTerm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Subscription dbEntity, Subscription entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
